f6aa7a13589b6ea1c9ce00de9f6393b2eb666124
akashkaran <karanakash8@gmail.com>
2017-12-15 14:44:56 +0530

    x

--- Scrapper/test.py
+++ Scrapper/test.py
@@ -12,7 +12,7 @@ def make_soup(url):
 
 csv="Test.csv"
 file = open(os.path.expanduser(csv),"wb")
-header="Player,Span,Mat,Inns,NO,Runs,R/N,HS,Ave,BF,SR,100,50,0,4s,6s"+"\n"
+header="Player,Span,Mat,Inns,NO,Runs,R/I,HS,Ave,BF,SR,100,50,0,4s,6s"+"\n"
 file.write(bytes(header,encoding="ascii",errors='ignore'))
 list_pl = make_soup("http://stats.espncricinfo.com/ci/engine/stats/index.html?agemax2=28;agemin2=22;ageval2=age;class=2;filter=advanced;orderby=runs;page=1;spanmax2=15+Dec+2017;spanmin2=15+Dec+2013;spanval2=span;team=1;team=12;team=15;team=2;team=25;team=29;team=3;team=4;team=40;team=5;team=6;team=7;team=8;team=9;template=results;type=batting;wrappertype=print")
 tdata=list_pl.findAll("table",{"class":"engineTable"})
