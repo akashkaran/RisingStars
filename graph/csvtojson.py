1b6fe9b14b922261d6cf76b67bfddb72b2274745
akashkaran <karanakash8@gmail.com>
2018-01-20 18:29:22 +0530

    graph
    generate graph based on ranks from csv

---
+++ graph/csvtojson.py
@@ -0,0 +1,25 @@
+import csv
+import sys
+import json
+
+fieldnames=["naive","cart","icc","score","name"]
+
+def convert(result):
+  csv_filename = "result.csv"
+  print ("Opening CSV file: "+csv_filename)
+  f=open(csv_filename, 'r')
+  csv_reader = csv.DictReader(f,fieldnames)
+  json_filename = csv_filename.split(".")[0]+".js"
+  print("Saving JSON to file: "+json_filename)
+  jsonf = open(json_filename,'w')
+  stvar="var jsonfile={jsonarray:"
+  end="};"
+  data = json.dumps([r for r in csv_reader])
+  jsonf.write(stvar)
+  jsonf.write(data)
+  jsonf.write(end)
+  f.close()
+  jsonf.close()
+
+if __name__=="__main__":
+    convert(sys.argv[1:])
