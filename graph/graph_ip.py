06307c72e96ef2619221402bd611370eb36e7063
Akash <karanakash8@gmail.com>
2018-02-01 22:20:29 +0530

    added function

--- graph/graph_ip.py
+++ graph/graph_ip.py
@@ -1,9 +1,11 @@
 from source.k import *
 
+def graphIp():
+    icc=readIcc() #reading icc top100
+    nblist=readNb() #reading nb top10
+    calist=readCart() #reading cart top10
 
-icc=readIcc() #reading icc top100
-nblist=readNb() #reading nb top10
-calist=readCart() #reading cart top10
-
-merge(icc,nblist,calist) #Merging Icc nb and cart ranks into single list
-convert(sys.argv[1:]) # to convert into json for graph input
+    merge(icc,nblist,calist) #Merging Icc nb and cart ranks into single list
+    convert(sys.argv[1:]) # to convert into json for graph input
+if __name__=="__main__":
+    graphIp()
